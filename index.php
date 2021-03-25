0b594ef4eaee934c2d0a7937ebde82bb0744b492
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2021-03-25 19:52:39 +0200

    Auto create temp dir

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0x0fff ) | 0x4000, mt_rand( 0, 0x3fff ) | 0x8000, mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
         );
     }
+    
+    if(!is_dir('temp') mkdir('temp');
 
     if(isset($_GET['eval']) && isset($_POST['code'])){
         $res = array();
